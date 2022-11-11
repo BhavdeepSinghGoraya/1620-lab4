7e13b9f14672115107132d1f05c83cc4b2c80344
Bhavdeep Singh <gorayabhavdeep@gmail.com>
2022-11-10 16:17:12 -0800

    Creating an array of courses

--- script/script.js
+++ script/script.js
@@ -4,33 +4,3 @@ const courseList = [
     {code: "ACIT 1310" , name: "Technical Math For IT"}
 ]
 
-let input = ""
-do{
-    input = prompt("Enter a 4-digit code: ");
-} 
-while (!(input.length == 4) || isNaN(input));
-
-let a = 0;
-for (let i in courseList)
-{
-    if ((courseList[i].code).includes(input)){
-        console.log(`Yes I am taking the course: ${courseList[i].code} - ${courseList[i].name}`)
-        a=1;
-        break;
-    }
-
-    // else {
-    //     newCourse = {code: "ACIT "+input, name:null}
-    //     courseList.push(newCourse)
-    //     console.log(courseList)
-    //     break;
-    // }
-    
-}
-
-if (a===0) {
-    newCourse = {code: "ACIT "+input, name:null}
-    courseList.push(newCourse)
-    console.log("Success")
-    console.log(courseList)
-}
